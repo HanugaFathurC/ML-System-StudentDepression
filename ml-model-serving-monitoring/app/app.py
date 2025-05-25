6c3d40d396315f2e546f386f4777c277ad1276eb
Hanuga Fathur C <21523245@students.uii.ac.id>
2025-05-25 18:26:59 +0800

    Fixing logic backend prediction

--- ml-model-serving-monitoring/app/app.py
+++ ml-model-serving-monitoring/app/app.py
@@ -9,31 +9,64 @@ from dotenv import load_dotenv
 load_dotenv()
 API_BASE_URL = os.getenv("API_BASE_URL", "http://localhost:8001")
 
+columns = [
+    "Gender", "Age", "Academic Pressure",
+    "Work Pressure", "CGPA", "Study Satisfaction",
+    "Job Satisfaction", "Sleep Duration",
+    "Dietary Habits", "Have you ever had suicidal thoughts ?",
+    "Work/Study Hours", "Financial Stress",
+    "Family History of Mental Illness"
+]
+
 st.set_page_config(page_title="Student Depression Detector")
 st.title("🧠 Student Depression Detector")
 
-columns = [
-    "Gender", "Age", "City", "Profession", "Academic Pressure",
-    "Work Pressure", "CGPA", "Study Satisfaction", "Job Satisfaction",
-    "Sleep Duration", "Dietary Habits", "Degree",
-    "Have you ever had suicidal thoughts ?", "Work/Study Hours",
-    "Financial Stress", "Family History of Mental Illness"
-]
 
-# Input user
-user_input = {}
-for col in columns:
-    user_input[col] = st.number_input(col, value=0.0)
+# Input for categorical features
+gender = st.selectbox("Gender", ["Male", "Female"])
+sleep_duration = st.selectbox("Sleep Duration",
+                              ["Less than 5 hours", "5-6 hours", "7-8 hours",
+                               "More than 8 hours", "Others"])
+suicidal_thoughts = st.selectbox("Have you ever had suicidal thoughts?", [
+    "Yes", "No"
+])
+dietary_habits = st.selectbox("Dietary Habits", [
+    "Healthy", "Moderate", "Unhealthy", "Others"
+])
+financial_stress_level = st.selectbox("Financial Stress Level 1.0 (Low) - 5.0 (High)", [
+    "1.0", "2.0", "3.0", "4.0", "5.0"
+])
+family_history = st.selectbox("Family History of Mental Illness", [
+    "Yes", "No"
+])
+
+# Input for numerical features
+age = st.number_input("Age", min_value=18, max_value=100, step=1, value=20)
+academic_pressure = st.slider("Academic Pressure (1-5)", 1, 5, 3)
+work_pressure = st.slider("Work Pressure (1-5)", 1, 5, 3)
+cgpa = st.number_input("CGPA", min_value=0.0, max_value=10.0, value=3.0, step=0.1)
+study_satisfaction = st.slider("Study Satisfaction (1-5)", 1, 5, 3)
+job_satisfaction = st.slider("Job Satisfaction (1-5)", 1, 5, 3)
+work_study_hours = st.number_input("Work/Study Hours per Day", min_value=0, max_value=16, step=1)
+
+# Save  to DataFrame
+data = pd.DataFrame([[gender, age, academic_pressure,
+                      work_pressure, cgpa, study_satisfaction, job_satisfaction, sleep_duration,
+                      dietary_habits, suicidal_thoughts, work_study_hours,
+                      financial_stress_level, family_history
+                      ]], columns=columns)
 
 if st.button("Predict"):
-    input_df = pd.DataFrame([user_input])
+    st.write("Input Data:")
+    st.dataframe(data)
+
 
     # Send to FastAPI /predict_queue
     try:
         payload = {
             "input": {
-                "columns": input_df.columns.tolist(),
-                "data": input_df.values.tolist()
+                "columns": data.columns.tolist(),
+                "data": data.values.tolist()
             }
         }
 
@@ -48,7 +81,7 @@ if st.button("Predict"):
             for _ in range(60):  # Max 60 attempts
                 result_resp = requests.get(f"{API_BASE_URL}/result/{job_id}")
                 result_data = result_resp.json()
-                if result_data.get("status") == "done":
+                if result_data.get("status") == "completed":
                     raw_result = result_data["result"]
                     result_value = raw_result[0] if isinstance(raw_result, list) else raw_result
                     label = "Depressed" if result_value == 1 else "Not Depressed"
--- ml-model-serving-monitoring/app/worker.py
+++ ml-model-serving-monitoring/app/worker.py
@@ -2,12 +2,14 @@ import redis
 import json
 import os
 import time
-
+import joblib
 import requests
 from dotenv import load_dotenv
+import pandas as pd
 
 
-# Load environment
+# === Load environment variables ===
+
 load_dotenv()
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
@@ -16,8 +18,49 @@ MODEL_API_URL = os.getenv("API_URL")
 if not MODEL_API_URL:
     raise ValueError("API_URL not set in .env file")
 
+
+# === Load pre-trained scaler and label encoders ===
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+PREPROCESS_DIR = os.path.join(BASE_DIR, "preprocessing", "output")
+if not os.path.exists(PREPROCESS_DIR):
+    raise FileNotFoundError(f"Preprocessing output directory {PREPROCESS_DIR} does not exist.")
+scaler_path = os.path.join(PREPROCESS_DIR, "scaler.joblib")
+if not os.path.exists(scaler_path):
+    raise FileNotFoundError(f"Scaler file {scaler_path} does not exist.")
+scaler = joblib.load(scaler_path)
+
+label_encoders_path = os.path.join(PREPROCESS_DIR, "label_encoders.joblib")
+if not os.path.exists(label_encoders_path):
+    raise FileNotFoundError(f"Label encoders file {label_encoders_path} does not exist.")
+label_encoders = joblib.load(label_encoders_path)
+
 # Init Redis
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+
+def preprocess_input(input_dict):
+    """
+    Preprocess the input data using the loaded scaler and label encoders.
+    """
+
+    df = pd.DataFrame([input_dict])  # convert to single-row DataFrame
+
+    # Format specific columns
+    if "Sleep Duration" in df.columns:
+        df["Sleep Duration"] = df["Sleep Duration"].apply(lambda x: f"'{x}'")
+
+    # Encode categorical features
+    for col, encoder in label_encoders.items():
+        if col in df.columns:
+            try:
+                df[col] = encoder.transform(df[col])
+            except ValueError:
+                raise ValueError(f"Invalid value for column '{col}': {df[col].values[0]}")
+
+    # Scale numerical features
+    numerical_cols = scaler.feature_names_in_
+    df[numerical_cols] = scaler.transform(df[numerical_cols])
+
+    return df
 
 def process_task():
     """
@@ -31,11 +74,17 @@ def process_task():
                 job_id = task["id"]
                 input_data = task["input"]
 
+                # Convert input data as dictionary
+                input_dict = dict(zip(input_data["columns"], input_data["data"][0]))
+
+                # Preprocess input data
+                input_data = preprocess_input(input_dict)
+
                 # Format payload
                 payload = {
                     "dataframe_split": {
-                        "columns": input_data["columns"],
-                        "data": input_data["data"]
+                        "columns": input_data.columns.tolist(),
+                        "data": input_data.values.tolist()
                     }
                 }
 
